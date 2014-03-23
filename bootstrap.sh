cb32e6c279d27cb4eb321d823f0a2f3f1aa417b9
Tushar Mathur <tusharmath@gmail.com>
2014-03-23 13:48:43 +0530

    1. Install node in non-interactive mode 2. Install Mongo in non interactive

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,12 @@ sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get install nodejs
+sudo apt-get install -y nodejs
 
 #Install MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 sudo apt-get update
-sudo apt-get install mongodb-10gen
+sudo apt-get install -y mongodb
 sudo service mongodb start
 
 #Install Node Global
