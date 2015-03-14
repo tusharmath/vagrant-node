7fc45fbe250440b979efc75c9f880310e98e6abe
Tushar Mathur <tusharmath@gmail.com>
2015-03-14 14:30:19 +0530

    --wip--

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #Install Nodejs
 sudo apt-get update
-sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
 
 #Install Curl
 sudo apt-get install curl
+
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+
+# Install Stable Node
+nvm use stable
+nvm alias default stable
 
 #Install MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
@@ -15,16 +18,8 @@ sudo apt-get install -y mongodb
 sudo service mongodb start
 
 #Install Node Global
-sudo npm install -g grunt-cli
-sudo npm install -g bower
-sudo npm install -g coffee-script
+sudo npm install -g grunt-cli gulp bower coffee-script node-dev nodemon
 
 #Install Git
 sudo apt-get install -y git-core
 git config --global color.ui true
-
-#Install Meteor
-#curl https://install.meteor.com | /bin/sh
-
-#Clone 
-git clone git@github.com:node-classifieds/node-classifieds.git ~/work-space
